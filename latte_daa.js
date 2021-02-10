1afaede75ebeb2226d6dd9b4edd811d5f80a0d0f
Cody Michaud <cody.r.michaud@gmail.com>
2021-02-09 23:23:02 -0800

    0

---
+++ latte_daa.js
@@ -0,0 +1,18 @@
+var submitBtn = document.getElementById("buttonContact")
+var emailInput = document.getElementById("inputEmail");
+var nameInput = document.getElementById("inputName");
+var reasonContact = document.getElementById("contactReason");
+
+submitBtn.addEventListener("click", function () {
+    if (!emailInput(
+        window.alert("You need to enter an email")
+    ));
+
+
+    window.alert("Thank you for contacting Latte Daa! Give us 36-48 hours to respond.")
+
+
+});
+
+
+
