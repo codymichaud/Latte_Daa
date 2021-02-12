7fcc286ac6abe718cce0bc15da1975f36824c4c5
Cody Michaud <cody.r.michaud@gmail.com>
2021-02-12 00:24:21 -0800

    added javascript functionality to notify user when the submission form has not been fully filled out. debugging reason for contact

--- latte_daa.js
+++ latte_daa.js
@@ -1,18 +1,36 @@
+//alert user when form isnt complete
+//link email to submit form so b&k will recieve contact submissions off Latte Daa
+//allow user to click on items for an ingredient and photo preview
+// allow user to customize drinks or menu items
+// allow user to add items to cart
+// allow users to delete items from cart
+// have cart total at bottom of page
+// allow users to edit items
+//
+
+
 var submitBtn = document.getElementById("buttonContact")
 var emailInput = document.getElementById("inputEmail");
 var nameInput = document.getElementById("inputName");
 var reasonContact = document.getElementById("contactReason");
+var inputtxt = document.getElementsByClassName("inputTxt");
+//contact_name.reason.contact_email.
 
-submitBtn.addEventListener("click", function () {
-    if (!emailInput(
-        window.alert("You need to enter an email")
-    ));
-
-
-    window.alert("Thank you for contacting Latte Daa! Give us 36-48 hours to respond.")
-
-
-});
+function validate_form() {
+    valid = true;
+    if (document.contact_form.contact_name.value == "") {
+        alert("Please fill in your Name.");
+        valid = false;
+    } else if (document.contact_form.contact_email.value == "") {
+        alert("Please fill enter your Email address.");
+        valid = false;
+    } else if (document.contact_form.contact_reason.value = "") { //<----figure out why it wont alert
+        alert("Please tell us why you are contacting us today.");
+        valid = false;
+    } else {
+        alert("Thank you for contacting Latte' Daa. We will reach out shortly.")
+    };
+};
 
 
 
